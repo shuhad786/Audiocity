8ed62685bfe295567d529421dbc89fe8d5d58966
shuhad786 <shuhad.loofer@gmail.com>
2025-06-19 12:36:17 +0200

    add exit button

--- app/src/main/java/com/example/playmanager/MainActivity.kt
+++ app/src/main/java/com/example/playmanager/MainActivity.kt
@@ -6,13 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.navigation.NavController
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.*
+import kotlin.system.exitProcess
+
 
 class MainActivity : ComponentActivity() {
     private val packingListViewModel: PackingListViewModel by viewModels()
@@ -39,18 +44,34 @@ fun MainScreen(navController: NavController, viewModel: PackingListViewModel) {
     var rating by remember { mutableStateOf("") }
     var comments by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
+
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+
+            // adds to playlist
             Button(onClick = { showDialog = true }) {
                 Text("Add to playlist")
             }
+            // navigates to next page
             Button(onClick = { navController.navigate("nextPage")}) {
                 Text("View playlist")
             }
-        }
+            // exits application
+            Button(
+                onClick = {
+                    // this closes the main activity
+                    MainActivity().finish()
+                    // this closes the application
+                    exitProcess(0)
+                }
+            ) {
+                Text("Exit")
+            }
 
         if (showDialog) {
             AlertDialog(
@@ -66,10 +87,30 @@ fun MainScreen(navController: NavController, viewModel: PackingListViewModel) {
                             value = artistName,
                             onValueChange = { artistName = it },
                             label = { Text("Artist Name") })
-                        OutlinedTextField(
-                            value = rating,
-                            onValueChange = { rating = it },
-                            label = { Text("Rating (1-5)") })
+                        if (rating != 6.toString() ) {
+                            OutlinedTextField(
+                                value = rating,
+                                onValueChange = { rating = it },
+                                label = { Text("Rating (1-5)") })
+                        }
+                        else {
+                            OutlinedTextField(
+                                value = rating,
+                                onValueChange = { rating = it },
+                                label = { Text("Rating (1-5)") },
+                                supportingText = {
+                                    //Text(errorMessage)
+                                    Text(
+                                        text = "Enter a number between 1 - 5",
+                                        style = TextStyle(
+                                            color = Color.Red,
+
+                                        )
+                                    )
+                                }
+                            )
+                        }
+
                         OutlinedTextField(
                             value = comments,
                             onValueChange = { comments = it },
@@ -79,12 +120,14 @@ fun MainScreen(navController: NavController, viewModel: PackingListViewModel) {
                 confirmButton = {
                     Button(onClick = {
                         if (songTitle.isNotBlank() && artistName.isNotBlank() && rating.toIntOrNull() != null && rating <= 5.toString()) {
-                            viewModel.items.add(mapOf(
-                                "songTitle" to songTitle,
-                                "artistName" to artistName,
-                                "rating" to rating,
-                                "comments" to comments
-                            ))
+                            viewModel.items.add(
+                                mapOf(
+                                    "songTitle" to songTitle,
+                                    "artistName" to artistName,
+                                    "rating" to rating,
+                                    "comments" to comments
+                                )
+                            )
                             showDialog = false
                             songTitle = ""
                             artistName = ""
@@ -92,7 +135,7 @@ fun MainScreen(navController: NavController, viewModel: PackingListViewModel) {
                             comments = ""
                         }
                     }){
-                        Text("Add")
+                        Text("Confirm")
                     }
                 }
             )
@@ -102,7 +145,9 @@ fun MainScreen(navController: NavController, viewModel: PackingListViewModel) {
 
 @Composable
 fun NextScreen(navController: NavController, viewModel: PackingListViewModel) {
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
         Button(onClick = { navController.navigateUp() }) {
             Text("Back")
         }
@@ -120,4 +165,3 @@ fun NextScreen(navController: NavController, viewModel: PackingListViewModel) {
         }
     }
 }
-
